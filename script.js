b1617d293175548c1b9098887965896e48fa4b4b
Dakota M <dakota.paige@hotmail.com>
2018-01-22 10:54:02 -0800

    Added update cart jquery & fixed css

--- script.js
+++ script.js
@@ -6,13 +6,26 @@ $(function() {
     wrapAround: true,
     resize: true
   })
+
+  $('#subscribe').on('submit', function(event) {
+    event.preventDefault();
+    if ( $('.input').val() == '') {
+      alert('Please submit a valid email address.');
+    } else {
+      alert('Thanks for subscribing!');
+    }
+  });
+
+  $('.cartUpdate').hide();
+
+  var currentItems = 1;
+
+  $('.addToCart').click(function() {
+    $('.cartUpdate').show();
+    $('.cartUpdate').html('<p>'+currentItems+'</p>');
+    currentItems++;
+    console.log(currentItems);
+  })
 });
 
-$('#subscribe').on('submit', function(event) {
-  event.preventDefault();
-  if ( $('.input').val() == '') {
-    alert('Please submit a valid email address.');
-  } else {
-    alert('Thanks for subscribing!');
-  }
-});
+
